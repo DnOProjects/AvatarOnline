2c78c276e3285a63ab74615e05ebaaea4f3fca47
Relton <ori.taylor@gmail.com>
2020-04-21 20:39:26 +0100

    Doing my job
    Menu is implemented and fully functional

--- main.lua
+++ main.lua
@@ -5,11 +5,12 @@ require 'src/classes/color'
 Constructors = {vector = ConstructVec, color = ConstructCol}
 --Global modules
 enet = require 'enet' --built-in module
-assets = require 'assets/assets'
+assets = require 'src/assets'
 debug, input, objMan, utils = require 'src/debugger', require 'src/input', require 'src/objectManager', require 'src/utils'
 client, server, bitser, net = require 'src/network/client', require 'src/network/server', require 'src/network/bitser', require 'src/network/net'
 graphics = require 'src/graphics/graphics'
 scale = require 'src/graphics/scale'
+ui = require 'src/graphics/ui'
 game = require 'src/game/game'
 
 local host = true --True: runs a server and a client; False: runs just a client
@@ -23,21 +24,29 @@ function love.load()
 	scale.load()
 	if host then server.start('*:'..port) end
 	client.connect(ip..':'..port)
+	ui.load()
 end
 
 function love.update(dt)
 	scale.update()
 	debug.update(dt)
-	if host then server.update(dt) end --Gets clients' requests, runs the game, sends instructions to clients
-	client.update(dt) --Gets server's instructions, sends requests to server
+	if currentPage =="inGame" then
+		if host then server.update(dt) end --Gets clients' requests, runs the game, sends instructions to clients
+		client.update(dt) --Gets server's instructions, sends requests to server
+	end
+	ui.update()
 end
 
 function love.draw()
 	love.graphics.push()
 	scale.draw()
 
-	client.draw() --Draws the game from the client's incomplete store of game objects
+	ui.drawBackgrounds()
+	if currentPage =="inGame" then
+		client.draw() --Draws the game from the client's incomplete store of game objects
+	end
   	debug.draw()
+  	ui.draw()
 
 	love.graphics.pop()
 end
